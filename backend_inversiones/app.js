f807113f03e9983b1c353d36a291c3674cccb42c
osvitap <johan.orellana.tapia@gmail.com>
2024-10-14 12:07:00 -0400

    nuevos cambios en el backend de movimientos

--- backend_inversiones/app.js
+++ backend_inversiones/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var movimientosRouter = require('./routes/movimientos');
 
 var app = express();
 
@@ -16,5 +17,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/movimientos', movimientosRouter);
 module.exports = app;
