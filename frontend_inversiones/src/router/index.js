9df15866b957aef31d4bf6964796b2cfb86f7680
CarlosCuxil <CarlosCuxil@users.noreply.github.com>
2024-10-15 16:10:52 -0600

    tardeee

--- frontend_inversiones/src/router/index.js
+++ frontend_inversiones/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Contacto from '../views/Contacto.vue'
-
+import LoginView from '@/views/LoginView.vue'
+ 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -11,11 +11,12 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/contacto',
-      name: 'contacto',
-      component: Contacto
+      path: '/login',
+      name: 'login',
+      component: LoginView
     }
+ 
   ]
 })
-
+ 
 export default router
