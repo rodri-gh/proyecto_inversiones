b1944a242f89e5bc30b1b55b6d910b74d40604cc
GisselaStephany <gissela_stephany@hotmail.com>
2024-10-15 11:51:51 -0400

    MODIFICADO ELIMINADO EN LINKS SETTINGSS E INVESTMENTS

--- backend_inversiones/routes/investments.js
+++ backend_inversiones/routes/investments.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var conexion = require('../database');
 
-// obtener ajustes
 router.get('/', function (req, res) {
 
     var query = 'SELECT * FROM inversiones;';
@@ -12,13 +11,13 @@ router.get('/', function (req, res) {
             console.log(error);
             res.status(500).send({
                 error: error,
-                message: 'Error al realizar la petición'
+                message: 'Error when making the request'
             })
         } else {
             console.log(results);
             res.status(200).send({
                 data: results,
-                message: 'Vista de Inversiones'
+                message: 'Investment view'
             });
         }
     });
@@ -27,70 +26,65 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res, next) {
     const { cliente_id, inversor_id, monto, tipo_ganancia, ganancia_estimada, fecha_deposito } = req.body;
 
-    var query = `INSERT INTO inversiones (cliente_id, inversor_id, monto, tipo_ganancia, ganancia_estimada, fecha_deposito) 
+    var query = `INSERT INTO inversiones (cliente_id, inversor_id, monto, tipo_ganancia, ganancia_estimada, fecha_deposito)
  VALUES ('${cliente_id}','${inversor_id}','${monto}','${tipo_ganancia}','${ganancia_estimada}','${fecha_deposito}');`;
 
     conexion.query(query, function (error, results, fields) {
-      if (error) {
-        console.log(error);
-        res.status(500).send({
-          error: error,
-          message: 'Error al realizar la petición'
-        })
-      } else {
-        console.log(results.insertId);
-        res.status(200).send({
-          data: results.insertId,
-          message: 'Inversión registrada correctamente'
-        });
-      }
+        if (error) {
+            console.log(error);
+            res.status(500).send({
+                error: error,
+                message: 'Error when making the request'
+            })
+        } else {
+            console.log(results.insertId);
+            res.status(200).send({
+                data: results.insertId,
+                message: 'Investment registered correctly'
+            });
+        }
     });
 
-  });
+});
 
 router.put('/:id', function (req, res, next) {
     const { cliente_id, inversor_id, monto, tipo_ganancia, ganancia_estimada, fecha_deposito } = req.body;
 
-
     var query = `UPDATE inversiones SET cliente_id="${cliente_id}", inversor_id="${inversor_id}", monto="${monto}", tipo_ganancia="${tipo_ganancia}", ganancia_estimada="${ganancia_estimada}", fecha_deposito="${fecha_deposito}" WHERE id = ${req.params.id};`;
 
     conexion.query(query, function (error, results, fields) {
-      if (error) {
-        console.log(error);
-        res.status(500).send({
-          error: error,
-          message: 'Error al realizar la petición'
-        })
-      } else {
-        console.log(results);
-        res.status(200).send({
-          data: results.insertId,
-          message: 'Inversión modificada correctamente'
-        });
-      }
+        if (error) {
+            console.log(error);
+            res.status(500).send({
+                error: error,
+                message: 'Error when making the request'
+            })
+        } else {
+            console.log(results);
+            res.status(200).send({
+                data: results.insertId,
+                message: 'The field was updated successfully'
+            });
+        }
     });
+});
 
-  });
-  
-  router.delete('/:id', function (req, res, next) {
-    var query = `DELETE FROM inversiones WHERE id = ${req.params.id};`;
+router.patch('/:id', function (req, res, next) {
 
+    const query = `UPDATE inversiones SET eliminado = CASE WHEN eliminado = '1' THEN '0' ELSE '1' END WHERE id = "${req.params.id}";`;
     conexion.query(query, function (error, results, fields) {
-      if (error) {
-        console.log(error);
-        res.status(500).send({
-          error: error,
-          message: 'Error al realizar la petición'
-        });
-      } else {
-        console.log(req.params.id);
-        res.status(200).send({
-          data: req.params.id,
-          message: 'Inversión eliminada correctamente'
-        });
-      }
+        if (error) {
+            console.log(error);
+            res.status(500).send({
+                error: error,
+                message: 'Error when making the request'
+            });
+        } else {
+            res.status(200).send({
+                data: results,
+                message: 'The field was updated successfully'
+            });
+        }
     });
-
-  });
-
+});
 module.exports = router;
--- backend_inversiones/routes/links.js
+++ backend_inversiones/routes/links.js
@@ -2,95 +2,86 @@ var express = require('express');
 var router = express.Router();
 var conexion = require('../database');
 
-// obtener ajustes
 router.get('/', function (req, res) {
 
-    var query = 'SELECT * FROM links;';
-
-    conexion.query(query, function (error, results) {
-        if (error) {
-            console.log(error);
-            res.status(500).send({
-                error: error,
-                message: 'Error al realizar la petición'
-            })
-        } else {
-            console.log(results);
-            res.status(200).send({
-                data: results,
-                message: 'Vista de Inversiones'
-            });
-        }
-    });
+  var query = 'SELECT * FROM links;';
+  conexion.query(query, function (error, results) {
+    if (error) {
+      console.log(error);
+      res.status(500).send({
+        error: error,
+        message: 'Error when making the request'
+      })
+    } else {
+      console.log(results);
+      res.status(200).send({
+        data: results,
+        message: 'Links view'
+      });
+    }
+  });
 });
 
 router.post('/', function (req, res, next) {
-    const { cliente_id, nombre, link, descripcion } = req.body;
+  const { cliente_id, nombre, link, descripcion } = req.body;
 
-    var query = `INSERT INTO links (cliente_id, nombre, link, descripcion) 
- VALUES ('${cliente_id}','${nombre}','${link}','${descripcion}');`;
+  var query = `INSERT INTO links (cliente_id, nombre, link, descripcion) VALUES ('${cliente_id}','${nombre}','${link}','${descripcion}');`;
 
-    conexion.query(query, function (error, results, fields) {
-      if (error) {
-        console.log(error);
-        res.status(500).send({
-          error: error,
-          message: 'Error al realizar la petición'
-        })
-      } else {
-        console.log(results.insertId);
-        res.status(200).send({
-          data: results.insertId,
-          message: 'Link registrado correctamente'
-        });
-      }
-    });
-
+  conexion.query(query, function (error, results, fields) {
+    if (error) {
+      console.log(error);
+      res.status(500).send({
+        error: error,
+        message: 'Error when making the request'
+      })
+    } else {
+      console.log(results.insertId);
+      res.status(200).send({
+        data: results.insertId,
+        message: 'Link was registered correctly'
+      });
+    }
   });
+
+});
 
 router.put('/:id', function (req, res, next) {
-    const { cliente_id, nombre, link, descripcion } = req.body;
+  const { cliente_id, nombre, link, descripcion } = req.body;
 
+  var query = `UPDATE links SET cliente_id="${cliente_id}", nombre="${nombre}", link="${link}", descripcion="${descripcion}" WHERE id = ${req.params.id};`;
 
-    var query = `UPDATE links SET cliente_id="${cliente_id}", nombre="${nombre}", link="${link}", descripcion="${descripcion}" WHERE id = ${req.params.id};`;
-
-    conexion.query(query, function (error, results, fields) {
-      if (error) {
-        console.log(error);
-        res.status(500).send({
-          error: error,
-          message: 'Error al realizar la petición'
-        })
-      } else {
-        console.log(results);
-        res.status(200).send({
-          data: results.insertId,
-          message: 'Link modificado correctamente'
-        });
-      }
-    });
-
+  conexion.query(query, function (error, results, fields) {
+    if (error) {
+      console.log(error);
+      res.status(500).send({
+        error: error,
+        message: 'Error when making the request'
+      })
+    } else {
+      console.log(results);
+      res.status(200).send({
+        data: results.insertId,
+        message: 'Link was updated successfully'
+      });
+    }
   });
-  
-  router.delete('/:id', function (req, res, next) {
-    var query = `DELETE FROM links WHERE id = ${req.params.id};`;
+});
+router.patch('/:id', function (req, res, next) {
 
-    conexion.query(query, function (error, results, fields) {
+  const query = `UPDATE links SET eliminado = CASE WHEN eliminado = '1' THEN '0' ELSE '1' END WHERE id = "${req.params.id}";`;
+  conexion.query(query, function (error, results, fields) {
       if (error) {
-        console.log(error);
-        res.status(500).send({
-          error: error,
-          message: 'Error al realizar la petición'
-        });
+          console.log(error);
+          res.status(500).send({
+              error: error,
+              message: 'Error when making the request'
+          });
       } else {
-        console.log(req.params.id);
-        res.status(200).send({
-          data: req.params.id,
-          message: 'Link eliminado correctamente'
-        });
+          res.status(200).send({
+              data: results,
+              message: 'Links was updated successfully'
+          });
       }
-    });
-
   });
-
+});
 module.exports = router;
