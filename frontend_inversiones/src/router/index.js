3a5a517744c59afcfd1cc35c8adab8a5cd1fe09a
CarlosCuxil <CarlosCuxil@users.noreply.github.com>
2024-10-15 15:35:56 -0600

    contacto

--- frontend_inversiones/src/router/index.js
+++ frontend_inversiones/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Contacto from '../views/Contacto.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/contacto',
+      name: 'contacto',
+      component: Contacto
+    }
+
     {
       path: '/about',
       name: 'about',
