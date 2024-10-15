e998401d657a9bf7abb7876de17720dc0838293f
CarlosCuxil <CarlosCuxil@users.noreply.github.com>
2024-10-15 15:56:45 -0600

    hola

--- frontend_inversiones/src/App.vue
+++ frontend_inversiones/src/App.vue
@@ -2,26 +2,13 @@
 import { RouterView } from "vue-router";
 </script>
 
-
 <template>
   <div>
-    <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/about">Contacto</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</div>
-  
+    <header></header>
+    <main class="container">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <style scoped>
