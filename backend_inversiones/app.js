eb61a98795f38834da2f0e671d2ae4c13a36b410
dan1398 <d1303gm@gmail.com>
2024-10-16 18:59:39 -0400

    proyects

--- backend_inversiones/app.js
+++ backend_inversiones/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var projectsRouter = require('./routes/projects');
 
 
 var app = express();
@@ -17,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/projects', projectsRouter);
 
 
 
