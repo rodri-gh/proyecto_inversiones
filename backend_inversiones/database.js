fd4cb478264db8c2a4526913080642d67d4ecc20
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2024-10-16 17:35:31 -0400

    dejando el proyecto en 0 y configurando la nueva BD

--- backend_inversiones/database.js
+++ backend_inversiones/database.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'proyecto_inversiones'
+  database: process.env.DB_DATABASE || 'minerals'
 });
 
 
