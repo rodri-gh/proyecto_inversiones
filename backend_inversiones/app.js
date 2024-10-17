a10590be80b2749eb7fe98569323da8ab0b591d0
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2024-10-17 12:28:44 -0400

    implementanto crud minerals

--- backend_inversiones/app.js
+++ backend_inversiones/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var projectsRouter = require('./routes/projects');
+const mineralsRouter = require('./routes/minerals');
 
 
 var app = express();
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
+app.use('/minerals', mineralsRouter);
 
 
 
