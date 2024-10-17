2a146d0eff9f750e7be4dd18328ce29780755e4b
dan1398 <d1303gm@gmail.com>
2024-10-17 12:05:58 -0400

    actualizado projects y investments

--- backend_inversiones/routes/investments.js
+++ backend_inversiones/routes/investments.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var connection = require('../database');
 
 router.get('/', function(req, res, next) {
-    const query = 'SELECT investments.*, projects.name AS project_name, users.name AS user_name FROM investments INNER JOIN projects ON investments.project_id = projects.id INNER JOIN users ON investments.user_id = users.id ;';
+    const query = 'SELECT investments.*, projects.name AS project_name, users.name AS user_name FROM investments INNER JOIN projects ON investments.project_id = projects.id INNER JOIN users ON investments.user_id = users.id ORDER BY investments.id ASC;';
 
     connection.query(query, function(error, results, fields) {
         if (error){
@@ -23,9 +23,9 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-    const {project_id, user_id, amount}=req.body;
+    const {project_id, user_id, amount, profit_percentage}=req.body;
 
-    const query = `INSERT INTO investments (project_id, user_id, amount, investment_date) VALUES ('${project_id}', '${user_id}', '${amount}', CURRENT_TIMESTAMP())`;
+    const query = `INSERT INTO investments (project_id, user_id, amount, investment_date, profit_percentage) VALUES ('${project_id}', '${user_id}', '${amount}', CURRENT_TIMESTAMP(), '${profit_percentage}');`;
     connection.query(query, function(error, results) {
         if (error){
             console.log(error);
@@ -45,9 +45,9 @@ router.post('/', function(req, res, next) {
 
 router.put('/:id', function(req, res, next) {
     const {id} = req.params;
-    const {project_id, user_id, amount}=req.body;
+    const {project_id, user_id, amount, profit_percentage}=req.body;
 
-    const query = `UPDATE investments SET project_id = '${project_id}', user_id = '${user_id}', amount = '${amount}' WHERE id = ${id};`;
+    const query = `UPDATE investments SET project_id = '${project_id}', user_id = '${user_id}', amount = '${amount}', profit_percentage = '${profit_percentage}' WHERE id = ${id};`;
     connection.query(query, function(error, results) {
         if (error){
             console.log(error);
