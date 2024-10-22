8ecdafd88cca24ada2ccdda6dd3fb078d5c4e08e
TicoPub <ticopub@gmail.com>
2024-10-22 18:02:02 -0400

    Nuevo Cambio rama

--- backend_inversiones/routes/contracts.js
+++ backend_inversiones/routes/contracts.js
@@ -1,120 +1,150 @@
 var express = require('express');
 var router = express.Router();
-var connection = require('../database');
+const conexion = require('../database');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
-router.get('/', function(req, res, next) {
+// Asegurar que el directorio donde se guardaran las imagenes existe
+const uploadDir = 'public/documents';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+// Configuración de Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir)
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+});
+
+const upload = multer({ storage: storage });
+
+router.get('/', function (req, res, next) {
     const query = 'SELECT * FROM contracts;';
-
-    connection.query(query, function(error, results, fields) {
-        if (error){
-            console.log(error);
-            res.status(500).json({
-                error: error,
-                message: 'Error in the query'
-            });
-        }else{
-            console.log(results);
-            res.status(200).json({
-                data: results,
-                message: 'List of contracts'
-            });
-        }
-    });
-});
-
-router.post('/', function(req, res, next) {
-    console.log("Request body: ", req.body);  // Imprime el cuerpo de la petición
-
-    const { project_id, user_id, investment_id, contract_code, contract_date, contract_file_path } = req.body;
-
-    if (!project_id || !user_id || !investment_id || !contract_code || !contract_date || !contract_file_path) {
-        return res.status(400).json({
-            message: "All fields are required"
+    conexion.query(query, function (error, results, fields) {
+      if (error) {
+        console.log(error);
+        res.status(500).json({
+          error: error,
+          message: 'Error in the query',
         });
-    }
-
-    const query = `INSERT INTO contracts (project_id, user_id, investment_id, contract_code, contract_date, contract_file_path) VALUES (?, ?, ?, ?, ?, ?)`;
-    connection.query(query, [project_id, user_id, investment_id, contract_code, contract_date, contract_file_path], function(error, results) {
-        if (error) {
-            console.log(error);
-            return res.status(500).json({
-                error: error.sqlMessage,
-                message: 'Error in the query'
-            });
-        }
+      } else {
+        console.log(results);
+        // para mostrar imagenes de la base de datos
+        results.forEach(element => {
+          if (element.contract_file_path) {
+            element.contract_file_path = `http://localhost:3000/documents/${element.contract_file_path}`;
+          }
+        })
         res.status(200).json({
-            data: results,
-            message: 'Contract created'
+          data: results,
+          message: 'Listing contracts',
         });
+      }
     });
 });
 
+router.post('/', upload.single('contract_file_path'), function (req, res, next) {
+    const { project_id, user_id, investment_id, contract_code, contract_date, } = req.body;
 
-
-// router.post('/', function(req, res, next) {
-
-//     const {project_id, user_id, investment_id, contract_code, contract_date, contract_file_path} = req.body; 
-    
-    
-//     const query = `INSERT INTO contracts (project_id, user_id, investment_id, contract_code, contract_date, contract_file_path) VALUES ("${project_id}", "${user_id}", "${investment_id}", "${contract_code}", "${contract_date}", "${contract_file_path}");`;
-//     connection.query(query, function(error, results) {
-//         if (error){
-//             console.log(error);
-//             res.status(500).json({
-//                 error: error,
-//                 message: 'Error in the query'
-//             });
-//         }else{
-//             console.log(results);
-//             res.status(200).json({
-//                 data: results,
-//                 message: 'contract created'
-//             });
-//         }
-//     });
-// });
-
-router.put('/:id', function(req, res, next) {
-    const {id} = req.params;
-    const {project_id, user_id, investment_id, contract_code, contract_date, contract_file_path} = req.body;
-
-
-    const query = `UPDATE contracts SET project_id = '${project_id}', user_id = '${user_id}', investment_id = '${investment_id}', contract_code = '${contract_code}', contract_date = '${contract_date}', contract_file_path = '${contract_file_path}' WHERE id = ${id};`;
-    connection.query(query, function(error, results) {
-        if (error){
-            console.log(error);
-            res.status(500).json({
-                error: error,
-                message: 'Error in the query'
-            });
-        }else{
-            console.log(results);
-            res.status(200).json({
-                data: results,
-                message: 'Contract updated'
-            });
-        }
+  
+    const contract_file_path = req.file ? `${req.file.filename}` : null;
+  
+    const query = `
+      INSERT INTO contracts (project_id,user_id,investment_id,contract_code,contract_date, contract_file_path)
+      VALUES ("${project_id}","${user_id}","${investment_id}", "${contract_code}", "${contract_date}", "${contract_file_path}");
+    `;
+  
+    conexion.query(query, function (error, results, fields) {
+      if (error) {
+        console.log(error);
+        res.status(500).json({
+          error: error,
+          message: 'Error in the query',
+        });
+      } else {
+        console.log(results);
+        res.status(200).json({
+          data: results,
+          message: 'Contract created',
+        });
+      }
     });
 });
 
-router.delete('/:id', function(req, res, next) {
-    const {id} = req.params;
-    const query = `UPDATE contracts SET deleted = CASE
-                    WHEN deleted = '1' THEN '0'
-                    ELSE '1'
-                    END WHERE id = ${id};`;
-    connection.query(query, function(error, results) {
-        if (error){
-            res.status(500).json({
-                message: 'Error updating motion status',
-                error
-            });
-        }else if(results.affectedRows === 0) {
-            res.status(404).json({ mensaje: 'Movement not found' });
-        } else {
-            res.status(200).json({ mensaje: 'Movement successfully updated' });
+router.put('/:id', upload.single('contract_file_path'), function (req, res, next) {
+    const contractId = req.params.id;
+    const { project_id, user_id, investment_id, contract_code, contract_date, contract_file_path } = req.body;
+  
+    const query = `SELECT contract_file_path FROM contracts WHERE id = "${contractId}";`;
+    conexion.query(query, function (error, results, fields) {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({
+          error: error,
+          message: 'Error retrieving contracts information',
+        });
+      }
+  
+      const currentContract = results[0];
+  
+  
+      let contract_file_path = currentContract.contract_file_path;
+      if (req.file) {
+        // Si se subió un nuevo documento, actualizar la ruta
+        contract_file_path = `${req.file.filename}`;
+  
+        // Eliminar el documento antiguo si existe
+        if (currentContract.contract_file_path) {
+          const oldImagePath = path.join(__dirname, '../public/documents', currentContract.contract_file_path);
+          fs.unlink(oldImagePath, (err) => {
+            console.error('Error deleting old document:', err);
+          });
         }
+      }
+  
+      const query = `
+        UPDATE contracts 
+        SET project_id = "${project_id}", user_id = "${user_id}", investment_id = "${investment_id}", contract_code = "${contract_code}", contract_date = "${contract_date}", contract_file_path = "${contract_file_path}"
+        WHERE id = "${contractId}";
+      `;
+  
+      conexion.query(query, (error, results) => {
+        if (error) {
+          console.error(error);
+          return res.status(500).json({
+            error: error,
+            message: 'Error updating contract',
+          });
+        }
+  
+        res.status(200).json({
+          message: 'Contract updated',
+        });
+      });
     });
+});
+
+router.patch('/:id', function (req, res, next) {
+  const contractId = req.params.id;
+
+  const query = `UPDATE minerals SET deleted = !deleted WHERE id = "${contractId}";`;
+  conexion.query(query, function (error, results, fields) {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({
+        error: error,
+        message: 'Error deleting contract',
+      });
+    }
+    res.status(200).json({
+      message: 'Contract deleted',
+    });
+  });
 });
 
 
