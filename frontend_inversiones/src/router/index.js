9017a5fa9884f96b856b5e6c763c90f4269f455e
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2024-10-23 12:39:10 -0400

    Actualizar ruta de administración de publicaciones

--- frontend_inversiones/src/router/index.js
+++ frontend_inversiones/src/router/index.js
@@ -57,8 +57,8 @@ const router = createRouter({
       component: CategoryPostView
     },
     {
-      path: '/posts',
-      name: 'posts',
+      path: '/posts-admin',
+      name: 'posts-admin',
       component: PostView
     },
     {
