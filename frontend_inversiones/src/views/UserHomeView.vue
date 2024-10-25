83d6eef93610722156e5167147d1cb6f7e62e784
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2024-10-25 19:40:26 -0400

    refactorizando rutas protegidas y ocultando el nav

---
+++ frontend_inversiones/src/views/UserHomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+    <RouterLink to="/login">Iniciar Sesion</RouterLink>
+  </main>
+</template>
