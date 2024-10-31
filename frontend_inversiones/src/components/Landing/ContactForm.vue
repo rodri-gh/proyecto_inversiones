34a830c64d7f3600fd81c5239df2a2cca484b42a
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2024-10-31 16:23:03 -0400

    Mejorar estilos y estructura de componentes: añadir estilos a botones, formularios de contacto y entradas, y actualizar el diseño del Blog.

--- frontend_inversiones/src/components/Landing/ContactForm.vue
+++ frontend_inversiones/src/components/Landing/ContactForm.vue
@@ -1,15 +1,41 @@
 <template>
-  <div>Formulario de contacto</div>
+  <div class="text-center mx-auto col-md-5 my-5">
+    <h1>¡Contáctanos Ya!</h1>
+    <div class="row">
+      <Input v-model="name" id="name" placeholder="Nombre" />
+
+      <Input v-model="email" id="email" placeholder="Correo" class="col-md-6" />
+      <Input
+        v-model="phone"
+        id="phone"
+        placeholder="Teléfono"
+        class="col-md-6"
+      />
+      <InputTextArea id="comments" v-model="comments" placeholder="Mensaje" />
+      <div>
+        <Button
+          text="Enviar Mensaje"
+          buttonClass="btn-primary py-3 px-4 fw-semibold"
+          @click="$emit('aqui el nombre de tu metodo que envia el formulario')"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import Input from "../base/Input.vue";
+import InputTextArea from "../base/InputTextArea.vue";
+import Button from "../base/Button.vue";
+const name = ref("");
+const email = ref("");
+const phone = ref("");
+const comments = ref("");
 </script>
 
-<style  scoped>
-div {
-  background-color: green;
-  color: white;
-  height: 1900px;
-  width: 100%;
+<style scoped>
+Button {
+  border-radius: 2rem !important;
 }
 </style>
