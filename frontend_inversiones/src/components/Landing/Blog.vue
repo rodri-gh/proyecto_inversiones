33539a34e14bc4db2476f06f6b2b3f3e20cde98c
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2024-10-31 10:57:13 -0400

    Añadir secciones de FAQ y Blog en UserHomeView, e integrar componentes correspondientes. Actualizar dependencias en package.json.

---
+++ frontend_inversiones/src/components/Landing/Blog.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="text-center">
+    <h1>Nuestro Blog</h1>
+    <Carousel v-bind="config">
+      <Slide v-for="(mineral, index) in mineralImages" :key="index">
+        <div class="carousel__item">
+          <img :src="mineral.src" :alt="mineral.alt" class="img-fluid" />
+          <button class="my-1">Leer mas...</button>
+        </div>
+      </Slide>
+
+      <template #addons>
+        <Navigation />
+      </template>
+    </Carousel>
+  </div>
+</template>
+
+<script setup>
+import Minerals1 from "@/assets/mineralsImage.jpeg";
+
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Navigation } from "vue3-carousel";
+
+const mineralImages = [
+  { src: Minerals1, alt: "image 1" },
+  { src: Minerals1, alt: "image 2" },
+  { src: Minerals1, alt: "image 3" },
+];
+
+const config = {
+  itemsToShow: 3.95,
+  wrapAround: true,
+  transition: 500,
+};
+</script>
+
+<style  scoped>
+button {
+  background-color: var(--button-primary);
+  color: white;
+  border: none;
+  border-radius: 70px;
+  padding: 10px 20px;
+  font-size: 16px;
+  height: 50px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  text-decoration: none;
+  line-height: 1.6;
+}
+
+button:hover {
+  background-color: var(--button-primary-hover);
+}
+
+.carousel__slide {
+  padding: 5;
+}
+
+.carousel__viewport {
+  perspective: 2000px;
+}
+
+.carousel__track {
+  transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+  transition: 0.5s;
+}
+
+.carousel__slide {
+  opacity: 0.9;
+  transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active ~ .carousel__slide {
+  transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+  opacity: 1;
+  transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide.carousel__slide--next {
+  opacity: 1;
+  transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+  opacity: 1;
+  transform: rotateY(0) scale(1);
+}
+</style>
---
+++ frontend_inversiones/src/components/Landing/FAQ.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="text-center">
+    <h1>Preguntas Frecuentes</h1>
+    <div class="accordion w-50 mx-auto" id="faqAccordion">
+      <div v-for="(faq, index) in FAQs" :key="index" class="accordion-item">
+        <h2 class="accordion-header" :id="'heading' + index">
+          <button
+            class="accordion-button collapsed py-4"
+            type="button"
+            data-bs-toggle="collapse"
+            :data-bs-target="'#collapse' + index"
+            :aria-expanded="false"
+            :aria-controls="'collapse' + index"
+            :class="{ collapsed: index !== 0 }"
+          >
+            {{ faq.question }}
+          </button>
+        </h2>
+        <div
+          :id="'collapse' + index"
+          class="accordion-collapse collapse"
+          :aria-labelledby="'heading' + index"
+          data-bs-parent="#faqAccordion"
+        >
+          <div class="accordion-body text-start">
+            {{ faq.answer }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const FAQs = [
+  {
+    question: "¿Qué son los minerales?",
+    answer:
+      "Los minerales son sustancias naturales, inorgánicas y de composición química definida.",
+  },
+  {
+    question: "¿Cómo se clasifican los minerales?",
+    answer:
+      "Los minerales se clasifican según su composición química y estructura cristalina.",
+  },
+  {
+    question: "¿Dónde se encuentran los minerales?",
+    answer:
+      "Los minerales se encuentran en todo tipo de formaciones geológicas, incluyendo rocas y suelos.",
+  },
+  {
+    question: "¿Por qué son importantes los minerales?",
+    answer:
+      "Los minerales son fundamentales para la industria y la tecnología moderna.",
+  },
+  {
+    question: "¿Cómo se extraen los minerales?",
+    answer:
+      "Los minerales se extraen mediante diversos métodos de minería, según su ubicación y características.",
+  },
+];
+</script>
+
+<style scoped>
+.accordion-button:not(.collapsed) {
+  background-color: white;
+  color: black;
+  box-shadow: none;
+}
+
+.accordion-button {
+  border: none;
+  border-radius: 1rem !important;
+}
+
+.accordion-item {
+  border: none;
+  border-radius: 1rem !important;
+  margin-bottom: 1rem;
+}
+
+.accordion-header {
+  margin-bottom: 1rem;
+}
+
+.accordion-body {
+  padding-left: 1.5rem;
+}
+</style>
