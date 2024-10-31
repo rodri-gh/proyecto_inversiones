9edd0533a5f196a78ab3b02012a90223834d68c0
CarlosCuxil <CarlosCuxil@users.noreply.github.com>
2024-10-31 00:26:13 -0600

    trabajando en contactos

--- frontend_inversiones/src/views/HomeView.vue
+++ frontend_inversiones/src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <script setup>
 import InvestmentsOfUser from '@/components/InvestmentsOfUser.vue';
+import Contacts from '@/components/Contacts.vue';
 const username = JSON.parse(localStorage.getItem('user'))?.username;
 </script>
 
 <template>
   <div class="m-5 text-center">
     <main>
-    <br>
-    <h2>Hola {{ username }}</h2>
-    <br>
-    <h5>estas son tus inversiones inversiones</h5>
-    <br>
-    <InvestmentsOfUser />
-  </main>
+      <br>
+      <h2>Hola {{ username }}</h2>
+      <br>
+      <h5>estas son tus inversiones inversiones</h5>
+      <br>
+      <InvestmentsOfUser />
+      <Contacts/>
+    </main>
+
   </div>
 </template>
