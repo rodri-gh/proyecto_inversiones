a1163664f685de9165504670d0cdb78ada09b783
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2024-11-07 18:18:31 -0400

    Haciento gestionable los FAQ

--- backend_inversiones/routes/faq.js
+++ backend_inversiones/routes/faq.js
@@ -8,46 +8,52 @@ import { created } from '../helpers/customMessage.js';
 
 const router = express.Router();
 router.get('/', async (req, res, next) => {
-    try {
-        const faqs = await Faq.findAll();
-        getHandleSuccess(200)(res, faqs);
-    } catch (error) {
-        getHandleError(error, res)
-    }
+  try {
+    const faqs = await Faq.findAll();
+    getHandleSuccess(200)(res, faqs);
+  } catch (error) {
+    getHandleError(error, res)
+  }
 });
 
 
 router.post('/', async (req, res, next) => {
-    const { ask, answer } = req.body;
-    try {
-        const faqs = await Faq.create({ ask, answer });
-        getHandleSuccess(201)(res, created.faq);
-    } catch (error) {
-        getHandleError(error, res)
-    }
+  const { ask, answer } = req.body;
+  try {
+    const faqs = await Faq.create({ ask, answer });
+    getHandleSuccess(201)(res, created.faq);
+  } catch (error) {
+    getHandleError(error, res)
+  }
 });
 
-router.put('/:id', async (req, res, next) =>{
-    const { id } = req.params;
-    const { ask, answer } = req.body;
-    try {
-        const faqs = await Faq.update({ask, answer }, { where: { id } });
-        getHandleSuccess(204)(res);
-    } catch (error) {
-        getHandleError(error, res)
-    }
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { ask, answer } = req.body;
+  try {
+    const faqs = await Faq.update({ ask, answer }, { where: { id } });
+    getHandleSuccess(204)(res);
+  } catch (error) {
+    getHandleError(error, res)
+  }
 });
 
 
-router.delete('/:id', async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        const [faqDeleted] = await Faq.update({ deleted: true }, { where: { id } });
-        verifyIfIdExists(faqDeleted);
-        getHandleSuccess(204)(res);
-    } catch (error) {
-        getHandleError(error, res)
+router.patch('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const faq = await Faq.findOne({ where: { id } });
+    if (!faq) {
+      return getHandleError(new Error('Faq not found'), res);
     }
+
+    const newDeletedStatus = faq.deleted ? 0 : 1;
+    await Faq.update({ deleted: newDeletedStatus }, { where: { id } });
+
+    getHandleSuccess(200)(res, `Faq ${newDeletedStatus ? 'deleted' : 'restored'} successfully`);
+  } catch (error) {
+    getHandleError(error, res)
+  }
 });
 
 export default router;
