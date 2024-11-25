70f35a8c8acb2dc465da4069b6718f4c2ece0521
Johan Orellana <83305827+OsviTap@users.noreply.github.com>
2024-11-25 13:00:10 -0400

    Update vite.config.js

--- frontend_inversiones/vite.config.js
+++ frontend_inversiones/vite.config.js
@@ -12,5 +12,16 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    target: 'esnext',
+    cssTarget: 'chrome61',
+    minify: false, // Deshabilita la minificación para reducir uso de CPU/RAM
+    chunkSizeWarningLimit: 1000, // Ajusta el límite del tamaño de los chunks
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   }
 })
